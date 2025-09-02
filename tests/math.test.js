4832c1d43575c54cd146f23b132ed01fbb069c61
Luis Medina <i202215560@cibertec.edu.pe>
2025-09-02 13:59:43 -0500

    test: add test for add(5,7) to force real implementation - RED

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,10 +2,16 @@
 import { add } from "../src/math.js";
 
 describe("Math module", () => {
-  test("adds two numbers correctly", () => {
+  test("adds 1 and 2 to make 3", () => {
     const result = add(1, 2);
     const expected = 3;
 
+    expect(result).toBe(expected);
+  });
+
+  test("adds 5 and 7 to make 12", () => {
+    const result = add(5, 7);
+    const expected = 12;
     expect(result).toBe(expected);
   });
 });
