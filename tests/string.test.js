5d20d2013831654723e3f61f948a39c0b860c2ef
Luis Medina <i202215560@cibertec.edu.pe>
2025-09-02 15:11:23 -0500

    test: add test for reverseSting('world') to force real implementation - RED

--- tests/string.test.js
+++ tests/string.test.js
@@ -6,7 +6,13 @@ describe("String Utilities", () => {
     const input = "hello";
     const expected = "olleh";
     const result = reverseString(input);
+    expect(result).toBe(expected);
+  });
 
+  test("reverse 'world' to 'dlrow'", () => {
+    const input = "world";
+    const expected = "dlrow";
+    const result = reverseString(input);
     expect(result).toBe(expected);
   });
 });
