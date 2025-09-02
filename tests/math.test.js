6a8fa40e8537bbd2cafd3abdb49e03ab34242ced
Luis Medina <i202215560@cibertec.edu.pe>
2025-09-02 14:36:43 -0500

    test: add failing test for subtract function - RED

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,5 @@
 // tests/math.test.js
-import { add } from "../src/math.js";
+import { add, subtract } from "../src/math.js";
 
 describe("Math module", () => {
   test("adds 1 and 2 to make 3", () => {
@@ -12,6 +12,12 @@ describe("Math module", () => {
   test("adds 5 and 7 to make 12", () => {
     const result = add(5, 7);
     const expected = 12;
+    expect(result).toBe(expected);
+  });
+
+  test("subtract 5 from 7 to make 2", () => {
+    const result = subtract(7, 5);
+    const expected = 2;
     expect(result).toBe(expected);
   });
 });
